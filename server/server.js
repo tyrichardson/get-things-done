033bacd58ef075e1c84a36badfee544a45efc1b5
Ty Richadson <tyLrichardson63@gmail.com>
2018-03-26 17:52:54 -0500

    second attempt to target Heroku to mLab database

--- server/server.js
+++ server/server.js
@@ -1,24 +1,23 @@
+//Require express -- find the express code that you need and assign it to a variable "app"
 const express = require('express');
 const app = express();
 
-//mongodb://<dbuser>:<dbpassword>@ds121665.mlab.com:21665/hadar-prime
+//Set up the port; if on Heroku, us port 4001; otherwise, use 5000
+const PORT = process.env.PORT || 5000;
 
-//if on Heroku, us port 4001; otherwise, use 5000
-const PORT = process.env.PORT || 4001;
+//Require body-parser -- find the body-parser code you need and assign it to a variable "bodyParser"
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
+//Require -- look in this file for code related to todo
 const todoRouter = require('./routes/todo.router');
 
+//Mongo code
 const mongoose = require('mongoose');
-const databaseURL = 'mongodb://localhost:27017/hadar';
 const databaseURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/hadar';
 
+//Connect to Mongo DB
 mongoose.connect(databaseURL);
 
-mongoose.connect(databaseUrl);
 mongoose.connection.on('connected', () => {
     console.log('mongoose connected pleasingly to mongod');
 });
@@ -27,10 +26,17 @@ mongoose.connection.on('error', (error) => {
 console.log('error connecting to the displeased mongod ', error);
 });
 
-app.use('/todo', todoRouter);
+//Configure body-parser for jQuery, then Angular
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Point to public folder containing static files
 app.use(express.static('server/public'));
 
+//Routes
+app.use('/todo', todoRouter);
+
+//Spin up server
 app.listen(PORT, () => {
     console.log('server listening on port: ', PORT);
 });
